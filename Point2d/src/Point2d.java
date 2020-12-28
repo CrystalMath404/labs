1521917ef7b19c6a5c593bfa140ff7561c30c345
CrystalMath404 <55714933+CrystalMath404@users.noreply.github.com>
2020-12-29 01:16:19 +0300

    Add files via upload

---
+++ Point2d/src/Point2d.java
@@ -0,0 +1,22 @@
+package Lab2;
+public class Point2d {
+    private double x,y,z;
+    public Point2d ( double xCoord, double yCoord) {
+        x = xCoord;
+        y = yCoord;
+    }
+    public double getX () {
+        return x;
+    }
+    public double getY () {
+        return y;
+    }
+    public void setX ( double xCoord) {
+        x = xCoord;
+    }
+    public void setY ( double yCoord) {
+        y = yCoord;
+    }
+
+}
+
---
+++ Point3d/src/Point3d.java
@@ -0,0 +1,4 @@
+public class Point3d extends Point2d{
+    public static void main(String[] args) {
+    }
+}
